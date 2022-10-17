02b978720586731f9d84b14f05877b8ef347c61f
Teofilus Yohanes Wowiling <tywowiling@gmail.com>
2022-10-17 11:37:38 +0700

    initial commit

---
+++ Week10/README.md
@@ -0,0 +1,10 @@
+# Introduction to Android Operating System
+
+![Programming Language](https://img.shields.io/badge/Programming%20Language-Java-red)
+![Code Editor](https://img.shields.io/badge/Code%20Editor-Android%20Studio-blue)
+![Original Date](https://img.shields.io/badge/Original%20Date-Apr%2019%2C%202021-important)
+
+## Description 
+
+Week ten project is an application to create Android Applications that can take advantage of *background processes* or *threads*
+such as: ***AsyncTask***, ***AsyncTaskLoader***, ***Intent Service***, ***Service***, and ***Bound Service***.
---
+++ Week10/W10c/app/src/main/java/umn/ac/id/w10c_33081/MainActivity.java
@@ -0,0 +1,65 @@
+package umn.ac.id.w10c_33081;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    CustomBoundService customBoundService;
+    boolean isBound = false;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            CustomBoundService.CustomLocalBinder binder =
+                    (CustomBoundService.CustomLocalBinder) service;
+            customBoundService = binder.getService();
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            isBound = false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent serviceIntent = new Intent(this, SimpleIntentService.class);
+        startService(serviceIntent);
+
+        Button btnStartService = findViewById(R.id.btnStartService);
+
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomService.class);
+                startService(intent);
+            }
+        });
+
+        Intent intent2 = new Intent(this, CustomBoundService.class);
+        bindService(intent2, serviceConnection, Context.BIND_AUTO_CREATE);
+
+        Button btnShowTime = findViewById(R.id.btnShowTime);
+        btnShowTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentTime =customBoundService.getCurrentTime();
+                Toast.makeText(getApplicationContext(), currentTime, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
